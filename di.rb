68c7d039a36db1eefe9b5de9102667cb058e2283
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-02 19:49:23 +0100

    Di stripped of initialization method. New method 'roll' added. New tests added, satisfies all three.

---
+++ di.rb
@@ -0,0 +1,8 @@
+class Di
+  
+  def roll
+    num = [1, 2, 3, 4, 5, 6]
+    return num
+  end
+
+end
--- spec/di_spec.rb
+++ spec/di_spec.rb
@@ -1,6 +1,19 @@
-require_relative '../di.rb
+require_relative '../di.rb'
 
-describe 'fizzbuzz' do
-  it 'returns "1"cd when passed 1' do
-    expect(fizzbuzz(1)).to eq 1
+describe Di do
+  it 'should be an instance of a "Di" object' do
+    expect(Di.new).to be_a Di
   end
+end
+
+describe Di do
+  it 'Di responds to roll' do
+    expect(subject).to respond_to :roll
+  end
+end
+
+describe Di do
+  it 'Di returns a number 1-6 when rolled' do
+    expect(subject.roll).to eq [1, 2, 3, 4, 5, 6]
+  end
+end
